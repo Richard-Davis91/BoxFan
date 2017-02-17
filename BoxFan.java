cf71b9f5503e5a7df3c46f9c92eb99463c9bd148
Richard-Davis91 <mogiz91@gmail.com>
2017-02-17 16:58:58 -0600

    adding files

---
+++ BoxFan.java
@@ -0,0 +1,14 @@
+package edu.jalc.boxfan;
+import edu.jalc.boxfan.FanCase;
+import edu.jalc.boxfan.FanBlades;
+import edu.jalc.boxfan.Motor;
+
+class BoxFan{
+  private BoxFan boxFan;
+  private FanCase fanCase;
+  private Motor motor;
+  private ControlSwitch controlSwitch;
+  private FanBlades fanBlades;
+
+  private BoxFan boxFan(){return this.boxFan;}
+}
---
+++ powercord/PowerCord.java
@@ -0,0 +1,13 @@
+package edu.jalc.boxfan;
+
+class PowerCord{
+  boolean pluggedIn;
+
+  private PowerCord(){pluggedIn = true;}
+
+  public boolean isPluggedIn(){return this.pluggedIn;}
+
+  public boolean plugIn(){return pluggedIn = true;}
+  public boolean unPlug(){return pluggedIn = false;}
+
+}
